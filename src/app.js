6ba92913b3afab5cecfcabc9f53eaebd4646e7b8
rob <robmathew21@gmail.com>
2024-08-29 21:37:35 -0500

    changed html to mustache, added css style and added jobs.js file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
 const app = express();
+const mustacheExpress = require('mustache-express');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS });
 });
 
 const port = process.envPORT || 3000;
